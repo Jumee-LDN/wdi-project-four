af24eb2846b66ba13224f588c47856d11aff1bd7
Jumee-LDN <jmlregister@gmail.com>
2018-12-05 21:39:23 +0000

    more initial files

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,5 @@
 const port = process.env.PORT || 4000;
-
-module.exports = { port };
+const env = process.env.NODE_ENV || 'dev';
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/whynot-${env}`;
+const secret = process.env.SECRET || 'always new';
+module.exports = { port, dbURI, secret, env };
