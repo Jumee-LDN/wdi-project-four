17d8f6b67b9f23d3e6ed14364ee08c914ffb2136
Jumee-LDN <jmlregister@gmail.com>
2018-12-09 23:36:53 +0000

    form error fixed

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -7,7 +7,7 @@ import FormButton from './formElements/FormButton';
 function ProjectForm({ handleChange, handleSubmit }) {
   return(
     <form onSubmit={handleSubmit} className="form-container">
-      <FormInput name="project title" type="text" placeholder="Your project name" handleChange={handleChange} />
+      <FormInput name="title" type="text" placeholder="Your project title" handleChange={handleChange} />
       <FormInput name="from" type="text" placeholder="Current role" handleChange={handleChange}/>
       <FormInput name="to" type="text" placeholder="Future role" handleChange={handleChange}/>
       <FormInput name="goal" type="number" placeholder="100 coins" handleChange={handleChange}/>
